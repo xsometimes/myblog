aefd769235e0d94e364568569c7eeaf499037043
xsometimes <1272549719@qq.com>
2020-12-22 16:34:06 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  theme: 'vdoing',
+  title: "passerby's blog",
+  description: '',
+  markdown: {
+    lineNumbers: true, // 代码行号
+  },
+  themeConfig: {
+    nav: [
+      { text: 'canvas', link: '/02.canvas', target: '_self', rel: '', items: [] }
+    ],
+    sidebar: {
+      mode: 'structuring', 
+      collapsable: true
+    },
+    sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2
+    searchMaxSuggestions: 10, // 搜索结果显示最大数
+    footer: {
+      // 页脚信息
+      createYear: 2020, // 博客创建年份
+      copyrightInfo:
+        'passerby | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+    }
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /hero.png
+heroText: RECORDING
+tagline: passerby's blog
+actionText: 快速上手 →
+actionLink: /zh/guide/
+features:
+- title: A Blog
+  details: 关于学习、记录、总结。
+- title: For Me
+  details: 不破不立。
+- title: For Interview
+  details: 暂缓。
+footer: MIT Licensed | Copyright © 2020-present passerby
+---
